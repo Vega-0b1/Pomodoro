baec51ea9eb0f9e2d9bd6f58b0b5236224692ec8
Unknown <howtonerdbuff@gmail.com>
2017-05-18 23:30:30 -0700

    when work timer ends break timer begins

--- scripts/countdownClassic/modifications.js
+++ scripts/countdownClassic/modifications.js
@@ -24,12 +24,41 @@ function setPom() {
   workValue = parseInt(pomIntervals[0].value);
   breakValue = parseInt(pomIntervals[1].value);
   longBreakValue = parseInt(pomIntervals[2].value);
-
   localStorage.userWorkValue = workValue;
+  workMode = false;
+  breakMode = true;
 }
 
+var workMode,breakMode;
+
 function decideTimer() {
+  if(workMode){
+    var minutes = workValue;
+    minutes = parseInt(minutes);
+    minutes = minutes*60000;
 
+    var seconds = 01;
+    seconds = parseInt(seconds);
+    seconds = seconds*1000;
 
+    globalTime = (minutes+seconds);
+
+    breakMode = true;
+    resume();
+  }
+  else if(breakMode){
+    var minutes = breakValue;
+    minutes = parseInt(minutes);
+    minutes = minutes*60000;
+
+    var seconds = 01;
+    seconds = parseInt(seconds);
+    seconds = seconds*1000;
+
+    globalTime = (minutes+seconds);
+
+    workMode = true;
+    resume();
+  }
 
 }
